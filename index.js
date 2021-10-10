15a2f1f257c3eb2a03116d0954909b37107a309d
Pedro Pina <pedrolpin4@Pedros-MacBook-Air.local>
2021-10-10 19:58:41 -0300

    moduling it

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import express from "express"
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
-app.listen(4000)
+const participant = {};
+const messagens = [];
+
+app.post("/login", (req, res) => {
+    console.log(req.body);
+    res.send({})
+})
+
+app.listen(4000);
