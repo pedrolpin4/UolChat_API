57cf5ffdf776303a0cf9c6da3fb5026e044b8581
Pedro Pina <pedrolpin4@Pedros-MacBook-Air.local>
2021-10-10 20:23:05 -0300

    providing the login settings

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 import express from "express"
+import cors from "cors"
 
 const app = express();
 app.use(cors());
+app.use(express.json())
 
 const participant = {};
 const messagens = [];
 
-app.post("/login", (req, res) => {
-    console.log(req.body);
-    res.send({})
+app.post("/", (req, res) => {
+    const user = req.body;
+    participant.name = user.name;
+    participant.lastStatus = Date.now();
+    console.log(participant);
+    user.name === "" ? res.status(400).send("Sorry, you have to type your pretty name") : res.status(200).send();
 })
 
 app.listen(4000);
